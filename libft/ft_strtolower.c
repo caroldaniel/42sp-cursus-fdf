e37cd3dfa51a42093a8e6c7ff95b70dacab430d3
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-09-18 16:51:30 -0300

    Bug in atoi_base

--- libft/ft_strtolower.c
+++ libft/ft_strtolower.c
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 11:59:46 by cado-car          #+#    #+#             */
-/*   Updated: 2021/09/18 12:03:26 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/09/18 16:51:22 by cado-car         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*ft_strtolower(char *str)
 {
 	size_t	i;
 
+	i = 0;
 	while (str[i])
 	{
 		str[i] = ft_tolower(str[i]);
