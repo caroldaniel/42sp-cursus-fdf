c5411f5a33d262bf9fddf877487ab98f1278b6a0
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-10-03 22:55:31 -0300

    Update menu

--- srcs/menu.c
+++ srcs/menu.c
@@ -27,5 +27,14 @@ void	print_menu(t_fdf *fdf)
 	
 	mlx_string_put(fdf->mlx, fdf->win, 50, y += 50, TEXT_DEFAULT, projection);
 	mlx_string_put(fdf->mlx, fdf->win, 50, y += 35, TEXT_DEFAULT, "Zoom: press '-' or '+'");
-	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Move: press ");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Move: press '←', '↑', '↓' or '→'");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Rotate X axis: press '2' or '8'");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Rotate Y axis: press '1' or '3'");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Rotate Z axis: press '4' or '6'");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Scale Altitude: press 'x' or 'z'");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 35, TEXT_DEFAULT, "To change projection view:");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Isometric: press 'I'");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Perspective: press 'P'");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 20, TEXT_DEFAULT, "Top View: press 'O'");
+	mlx_string_put(fdf->mlx, fdf->win, 50, y += 35, TEXT_DEFAULT, "Reset view: press 'R'");
 }
